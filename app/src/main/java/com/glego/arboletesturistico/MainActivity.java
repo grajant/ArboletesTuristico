a4ba6fcbcd1609f2b3c13ce3d216311b7fb92484
HOME-I <glego40_@hotmail.com>
2017-03-06 20:55:23 -0500

    Initial commit

---
+++ app/src/main/java/com/glego/arboletesturistico/MainActivity.java
@@ -0,0 +1,49 @@
+package com.glego.arboletesturistico;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.Interpolator;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String username;
+    private String email;
+    private Bundle extras;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        extras = getIntent().getExtras();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()){
+            case R.id.menu_profile:
+                intent = new Intent(MainActivity.this, ProfileActivity.class);
+                intent.putExtra("username", extras.getString("username"));
+                intent.putExtra("email", extras.getString("email"));
+                startActivity(intent);
+                break;
+            case R.id.logout_menu:
+                intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/com/glego/arboletesturistico/ProfileActivity.java
@@ -0,0 +1,25 @@
+package com.glego.arboletesturistico;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private TextView usernameTextView;
+    private TextView emailTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        usernameTextView = (TextView) findViewById(R.id.username);
+        emailTextView = (TextView) findViewById(R.id.email);
+
+        Bundle extras = getIntent().getExtras();
+
+        usernameTextView.setText(extras.getString("username"));
+        emailTextView.setText(extras.getString("email"));
+    }
+}
