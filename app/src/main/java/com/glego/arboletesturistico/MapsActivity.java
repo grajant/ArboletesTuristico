1ed08e54afd5c956fb99b30d2f2fc8a3e070f7d1
mint <glego40_@hotmail.com>
2017-03-27 17:40:13 -0500

    Version_4.0 Maps and Shared preferences
    Initial commit

--- app/src/main/java/com/glego/arboletesturistico/MapsActivity.java
+++ app/src/main/java/com/glego/arboletesturistico/MapsActivity.java
@@ -10,7 +10,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends DrawerActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
@@ -40,7 +40,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15));
+    }
+
+    @Override
+    protected boolean useToolbar() {
+        return false;
     }
 }
